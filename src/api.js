8bac9eb4f6aed7a6d3e924307902ae424a934a9d
rawhite22 <robert.a.white625@gmail.com>
2020-11-15 15:03:21 -0500

    2. setting up our api

---
+++ src/api.js
@@ -0,0 +1,29 @@
+// Variable Logic
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+// Variables
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+// base url
+const base_url = 'https://api.rawg.io/api/';
+// popular games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+export const popularGamesURL = () => `${base_url}${popular_games}`;
