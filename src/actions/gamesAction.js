a6ac70cba05af9b7bbfbb5849252c59569b69115
rawhite22 <robert.a.white625@gmail.com>
2020-11-15 23:12:34 -0500

    6. fetching other games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
-import { popularGamesURL } from '../api';
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from '../api';
 
 // Action Creator
 
 export const loadGames = () => async (dispatch) => {
   const popularGamesData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const upcomingGamesData = await axios.get(upcomingGamesURL());
   dispatch({
     type: 'FETCH_GAMES',
-    payload: { popular: popularGamesData.data.results },
+    payload: {
+      popular: popularGamesData.data.results,
+      upcoming: upcomingGamesData.data.results,
+      newGames: newGamesData.data.results,
+    },
   });
 };
--- src/api.js
+++ src/api.js
@@ -24,6 +24,11 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 // base url
 const base_url = 'https://api.rawg.io/api/';
-// popular games
+// games url
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+// urls
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
