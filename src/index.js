790c33086e1bb3fd88fbfcb43a014cdd755ef5d8
rawhite22 <robert.a.white625@gmail.com>
2020-11-15 19:46:48 -0500

    4. action creators,redux,thunk

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // redux setup
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnchancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
